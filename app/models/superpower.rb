a82a38d991972a55b6e33b49f46a570265554df1
Vibraniumforge <gregm9999@gmail.com>
2019-01-17 18:55:30 -0800

    removed navigation

--- app/models/superpower.rb
+++ app/models/superpower.rb
@@ -4,16 +4,5 @@ class Superpower < ActiveRecord::Base
 
     validates :name, presence: true
     validates :character, presence: true
-   
-    def character_attributes(character_attributes_attributes)
-        character_attributes_attributes.values.each do |character_attributes|
-          if character_attributes[:name].present?
-            character= Character.find_or_create_by(character_attributes)
-            if !self.character = character
-              self.build_character(:name => character[:name])
-            end
-          end
-        end
-      end
 
 end
