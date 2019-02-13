7bd5b484419370d2310767495818c09072a639ba
Vibraniumforge <gregm9999@gmail.com>
2019-02-12 19:38:56 -0800

    refactored 2 serializers

--- app/serializers/character_serializer.rb
+++ app/serializers/character_serializer.rb
@@ -1,6 +1,6 @@
 class CharacterSerializer < ActiveModel::Serializer
   attributes :id, :name, :alias
   has_many :battles
-  has_many :movies, through: :battles
+  has_many :movies
   has_many :superpowers
 end
--- app/serializers/movie_serializer.rb
+++ app/serializers/movie_serializer.rb
@@ -1,5 +1,5 @@
 class MovieSerializer < ActiveModel::Serializer
   attributes :id, :name, :year, :poster
   has_many :battles
-  has_many :characters, through: :battles
+  has_many :characters
 end
